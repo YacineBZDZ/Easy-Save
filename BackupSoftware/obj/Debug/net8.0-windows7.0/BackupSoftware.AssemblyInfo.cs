78fc3d12c050c3a6aeed603dee9b750aa8f0db08
YacineBZDZ <jakmca18@gmail.com>
2023-12-11 10:39:06 +0100

    All-Functionality-Made

--- BackupSoftware/obj/Debug/net8.0-windows7.0/BackupSoftware.AssemblyInfo.cs
+++ BackupSoftware/obj/Debug/net8.0-windows7.0/BackupSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackupSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe3b0dfabaa1f2213799e541aa7b816e231bb99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackupSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackupSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
